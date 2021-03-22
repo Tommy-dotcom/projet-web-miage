ad1cdddd5ed4faf6f85bf34f7a110fa8697ebada
Thomas <totolamothe65@gmail.com>
2021-03-22 16:31:38 +0100

    Swagger basic route

--- back-end/index.js
+++ back-end/index.js
@@ -6,29 +6,6 @@ let express = require('express'),
 const swaggerUi = require('swagger-ui-express'),
 swaggerDocument = require('./swagger.json');
 
-// Options pour la documenation Swagger
-const options = {
-	definition: {
-		openapi: '3.0.0',
-		info: {
-			title: "Application Web MIAGE",
-			version: '0.1.0',
-			description: "Documentation de l'API de l'application web de la MIAGE",
-			contact: {
-				name: 'Thomas LAMOTHE',
-				url: 'https://thomaslamothe.com',
-				email: 'thomaslamothe@free.fr'
-			},
-		},
-		servers: [
-			{
-				url: 'http://localhost:8000'
-			}
-		]
-	},
-	apis: ['./routes/*.js'],
-};
-
 // Création de l'application Express
 const port = 8000;
 const app = express();
